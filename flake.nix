bb8354d2de65f00ec5e8522845ce04a71b452122
Tom Ford <tfordy63@gmail.com>
2025-10-24 10:25:02 +0200

    still not needed

--- flake.nix
+++ flake.nix
@@ -20,17 +20,10 @@
           version = "0.1.0";
           src = ./.;
 
-          # First build: this will fail and print the correct hash.
-          # Use a typed fake SRI so evaluation doesn't bail out early.
           vendorHash = "sha256-+5/WOmVNAM+JRsh6KtzdQ+sHlcShnP/csIkwiwJ1WmI=";
           go = pkgs.go;
 
-          # CGO/sqlite
           env.CGO_ENABLED = "1";
-          nativeBuildInputs = [pkgs.pkg-config];
-          buildInputs = [pkgs.sqlite];
-
-          # Build the main package at repo root to a 'bingo' binary
           subPackages = ["."];
         };
       in {
@@ -52,7 +45,6 @@
             gopls
             gcc
             sqlite
-            pkg-config
           ];
           env = {
             CGO_ENABLED = "1";
